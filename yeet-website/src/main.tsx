3580ec24ed33d13777b656023b3cecb8921e7c33
yethikrishna <105283902+yethikrishna@users.noreply.github.com>
2025-05-31 10:03:06 +0530

    🚀 Create minimal main.tsx entry point

--- yeet-website/src/main.tsx
+++ yeet-website/src/main.tsx
@@ -1,13 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { ErrorBoundary } from './components/ErrorBoundary.tsx'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
